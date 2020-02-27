79047179ae43e3910309579b9dc236c3b7ca88db
fdearmas96 <47925506+fdearmas96@users.noreply.github.com>
2020-02-27 20:29:39 -0300

    Update ConsultarPublicacion.php

--- ConsultarPublicacion.php
+++ ConsultarPublicacion.php
@@ -11,18 +11,19 @@ $json=array();
 				
 		$conexion = mysqli_connect($hostname_localhost,$username_localhost,$password_localhost,$database_localhost);
 
-		$consulta="select Pub_ID, Pub_Titulo, Pub_Desc, Pub_fecha, Pub_img, Pub_Contacto from publicacion where Pub_ID= '{$Pub_ID}'";
+		//$consulta="select Pub_ID, Pub_Titulo, Pub_Desc, Pub_fecha, Pub_img, Pub_Contacto from publicacion where Pub_ID= '{$Pub_ID}'";
+		$consulta="select * from publicacion where Pub_ID= '{$Pub_ID}'";
 		$resultado=mysqli_query($conexion,$consulta);
 			
 		if($registro=mysqli_fetch_array($resultado)){
 			$result["Pub_ID"]=$registro['Pub_ID'];
 			$result["Pub_Titulo"]=$registro['Pub_Titulo'];
 			$result["Pub_Desc"]=$registro['Pub_Desc'];
-			$result["Pub_img"]=$registro['Pub_img']);//base64_encode($registro['Pub_img']);
+			$result["Pub_img"]=$registro['Pub_img'];//base64_encode($registro['Pub_img']);
 			$result["Pub_Contacto"]=$registro['Pub_Contacto']; 
 			$json['publicacion'][]=$result;
 		}else{			
-			$resultar["success"]=0;
+			$resultar["success"]=$Pub_ID;
 			$resultar["message"]='No existe 2';
 			$json['publicacion'][]=$resultar;
 		}
