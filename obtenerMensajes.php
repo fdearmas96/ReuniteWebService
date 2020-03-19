bddb2d1c659301b7aa5ccf239ac962b365712e59
metroid112 <federico.aguilera112@gmail.com>
2020-03-19 10:54:54 -0300

    ws obtener mensaje

--- obtenerMensajes.php
+++ obtenerMensajes.php
@@ -1,45 +1,37 @@
 <?PHP
-/*$hostname_localhost ="localhost";
-$database_localhost ="reunitebd";
-$username_localhost ="root";
-$password_localhost ="";*/
+	require('conexionSql.php');
 
-require('conexionSql.php');
+	// Función para debuggear
+	function console_log( $data ){
+	  echo '<script>';
+	  echo 'console.log('. json_encode( $data ) .')';
+	  echo '</script>';
+	}
 
-// Función para debuggear
-function console_log( $data ){
-  echo '<script>';
-  echo 'console.log('. json_encode( $data ) .')';
-  echo '</script>';
-}
+	$json=array();
 
-$json=array();
-				
-	//$conexion = mysqli_connect($hostname_localhost,$username_localhost,$password_localhost,$database_localhost);
-	
 	if ($conexion->connect_error) {
 		die("Conexión fallida: " . $conexion->connect_error);
 	}
-	if($_GET["usuario"]!=null){
-		$usuario=$_GET["usuario"];	
-		$consulta="select Pub_ID, Pub_Titulo, Pub_Desc, Pub_img, Pub_Contacto from publicacion where Usuario_ID = '{$usuario}'";}
-	else{
-		$consulta="select Pub_ID, Pub_Titulo, Pub_Desc, Pub_img, Pub_Contacto from publicacion";	
+
+	if($_GET["usuario"] != null){
+		$usuario = $_GET["usuario"];	
+		$consulta = "select Msg_ID, Msg_Mensaje, Usuario_ID1, Usuario_ID2 from mensajes inner join msguser on mensajes.Msg_ID = msguser.Msg_ID where Usuario_ID2 = '{$usuario}'";
+	} else {
+		$consulta = "select Msg_ID, Msg_Mensaje, Usuario_ID1, Usuario_ID2 from mensajes inner join msguser on mensajes.Msg_ID = msguser.Msg_ID";	
 	}
-	//$consulta="select Pub_ID, Pub_Titulo, Pub_Desc, Pub_img, Pub_Contacto from publicacion";
-	$resultado= $conexion->query($consulta); // mysqli_query($conexion,$consulta);
+
+	$resultado = $conexion->query($consulta);
 	
 	if ($resultado->num_rows > 0) {
 		while($registro = $resultado->fetch_assoc()) {
 			
-			$result["Pub_ID"]=$registro['Pub_ID'];
-			$result["Pub_Titulo"]=$registro['Pub_Titulo'];
-			$result["Pub_Desc"]=$registro['Pub_Desc'];
-			// $result["Pub_img"]=base64_encode($registro['Pub_img']);
-			$result["Pub_img"]=$registro['Pub_img'];
-			$result["Pub_Contacto"]=$registro['Pub_Contacto']; 
+			$result["Msg_ID"] = $registro['Msg_ID'];
+			$result["Msg_texto"] = $registro['Msg_Mensaje'];
+			$result["Msg_usersend"] = $registro['Usuario_ID1'];
+			$result["Msg_userreceive"] = $registro['Usuario_ID2'];
 			
-			$json['publicacion'][]=$result;
+			$json['mensajes'][] = $result;
 		}
 	} else {
 		echo "0 results";
