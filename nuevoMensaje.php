884eb05bf2879741d7ad6c20906ef0616ba2ec3a
metroid112 <federico.aguilera112@gmail.com>
2020-03-19 11:00:52 -0300

    Nuevo msj php

---
+++ nuevoMensaje.php
@@ -0,0 +1,36 @@
+<?PHP
+    require('conexionSql.php');
+
+	$usersend   = $_POST["usersend"];
+	$userreceive = $_POST["userreceive"];
+	$messagebody = $_POST["messagebody"];
+	
+	//Saco el último ID
+	$consulta = "SELECT Msg_ID FROM mensajes order by Msg_ID desc";
+	$resultado = $conexion->query($consulta);
+	$maximoencontrado = 0;
+	if($registro = $resultado->fetch_array()){
+		$maximoencontrado = $registro['Msg_ID'];		
+	}
+	$maximoencontrado = $maximoencontrado + 1;
+
+	$nombreImg = $maximoencontrado;	
+    $sqlinsert = "INSERT INTO mensajes(Msg_ID, Msg_Mensaje) 
+    VALUES ('{$maximoencontrado}', '{$messagebody}')";
+    $resultado_insert = $conexion->query($sqlinsert);
+
+    $sqlinsert = "INSERT INTO msguser(Msg_ID, Usuario_ID1, Usuario_ID2) 
+    VALUES ('{$maximoencontrado}', '{$usersend}', '{$userreceive}')";
+    $resultado_insert = $conexion->query($sqlinsert);
+    
+    $sqlselect = "Select * from publicacion where Pub_Id = '{$maximoencontrado}'";
+    $resultado = $conexion->query($sqlselect);
+    
+	if($registro = $resultado->fetch_array()) {
+	    echo "registra";
+		mysqli_close($conexion);
+	} else {
+	    mysqli_close($conexion);
+        echo "noRegistra";
+	}
+?>
